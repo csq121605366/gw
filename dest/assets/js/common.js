17e1e73e6dc89f087c595b45cf46bd93f113d088
caishangqing <121605366@qq.com>
2017-06-01 22:47:18 +0800

    inf

--- dest/assets/js/common.js
+++ dest/assets/js/common.js
@@ -1,29 +1,44 @@
 define('common', function(require, exports, module) {
 
-  $(function() {
+  $(function () {
+  
   
       // 头部
       $(".button-collapse").sideNav();
   
+      $('.materialboxed').materialbox();
   
       var $map=$('.m-map');
+  
       if($map.length){
       	
-      	function initialize() {  
-      		var mp = new BMap.Map('map'); 
-      		console.log(mp)
-      		mp.centerAndZoom(new BMap.Point(121.491, 31.233), 11);  
-      	}  
       	
-      	function loadScript() {  
-      		var script = document.createElement("script");  
-      		script.src = "http://api.map.baidu.com/api?v=2.0&ak=MhdTi8ClYYqNTLG6TXZP9edTXZLDGGsb&callback=initialize";
-      		document.body.appendChild(script);  
-      	}  
-      	window.onload = loadScript;  
+        var map = new BMap.Map('map'); 
+        var point = new BMap.Point(114.411664, 30.512013);  
+        map.centerAndZoom(point, 15);
+        map.enableScrollWheelZoom(true);
+        var marker = new BMap.Marker(point);  
+        // 创建标注
+        map.addOverlay(marker);               
+      // 将标注添加到地图中
+      marker.setAnimation(BMAP_ANIMATION_BOUNCE); 
+      //跳动的动画
+      // 覆盖区域图层测试
+      map.addTileLayer(new BMap.PanoramaCoverageLayer());
+      var stCtrl = new BMap.PanoramaControl(); 
+      //构造全景控件
+      stCtrl.setOffset(new BMap.Size(20, 20));
+      map.addControl(stCtrl);
+      //添加全景控件
+      map.addControl(new BMap.NavigationControl());    
+      map.addControl(new BMap.ScaleControl());    
+      // map.addControl(new BMap.OverviewMapControl());    
+      // map.addControl(new BMap.MapTypeControl());    
+      map.setCurrentCity("武汉"); 
   
   
-      }
+  }
+  
   
   
   })
